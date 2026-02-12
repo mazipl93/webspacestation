629f341180c984a931460dc4a182e219e2441ca5
mazipl93 <dawidm948@gmail.com>
2026-02-12 18:55:40 +0100

    Delete pobierz.php

--- pobierz.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-$url = $_GET['url'];
-if (strpos($url, 'https://services.swpc.noaa.gov/') === 0) {
-    echo file_get_contents($url);
-}
-?>
